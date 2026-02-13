d497e87e8fb5b766ea8a43f2306ea400de4ad0bc
RiceZero <info@ricezero.fun>
2026-02-13 23:41:55 +0900

    add design file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use sysinfo::{System, SystemExt, ProcessExt};
+use serde::Serialize;
 
 fn main() {
     tasklite_lib::run()
